560a6eb8b0b94fb86abbd1e40a0af3d2d637a1f8
Anastasia Zinchenko <nastyagun1993@yandex.ru>
2021-05-19 19:05:19 +0300

    333

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,20 @@ const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/not-found-err');
 const { validateUser, validateCreateUser } = require('../middlewares/validation');
-const {
-  notFoundError,
-} = require('../utils/constants');
+// const { CLIEngine } = require('eslint');
+// // const {
+//   notFoundError,
+// } = require('../utils/constants');
 
 router.post('/signup', validateCreateUser, createUser);
 router.post('/signin', validateUser, login);
 router.use(auth);
 router.use('/users', auth, userRouter);
 router.use('/movies', auth, moviesRouter);
-router.use('*', (res, req, next) => {
-  next(new NotFoundError(notFoundError));
+router.use('*', () => {
+  console.log('asdasda');
+  throw new NotFoundError({ message: 'NOT_FOUND_ERROR' });
+  // next(new NotFoundError(notFoundError));
 });
 
 module.exports = router;
